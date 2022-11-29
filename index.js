121e41caee345c4d86409698c9b64be98b059df1
GermanoCostaUX <cgermano.fcosta.nnt@gmail.com>
2022-11-29 13:32:38 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+ReactDOM.render(<h1>Hello, everyone</h1>, document.getElementById("root"))
+ReactDOM.render(<p>My name is Germano Costa</p>, document.querySelector("#root-1"))
+ReactDOM.render(<p>This is my first React Code</p>, document.querySelector("#root-2"))
+ 
